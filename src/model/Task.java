1d5f7d4b10402fd0b92f6c4db39ace3a70ddc682
yh0416 <yh990416@gmail.com>
2024-10-09 15:20:43 -0400

    The model Task.class has been completed. In there attribute status is enum with PENDING,IN_PROGRESS, COMPLETED; and the dealine is in LocalDate type to better suit the calender front-end data

---
+++ src/model/Task.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Task {
+
+    private String taskName;
+    private String description;
+    private String priority;
+    private LocalDate deadline;
+    private TaskStatus status;
+
+    //constructor
+    public Task(String taskName, String description, LocalDate deadline,
+            String priority, TaskStatus status) {
+        this.taskName = taskName;
+        this.description = description;
+        this.priority = priority;
+        this.deadline = deadline;
+        this.status = status;
+
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+}
