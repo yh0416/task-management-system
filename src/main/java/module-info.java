6a1975e7b5a3d970f413b8f6056b7f0ba0b2d642
natasha-kiara <kiara.n@northeastern.edu>
2024-10-09 21:10:28 -0400

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.natasha.taskmanagementapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens controllers to javafx.fxml;
-    exports controllers;
-}
